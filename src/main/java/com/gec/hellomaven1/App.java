dee05a0510c56b1a2eb1f2241a4c08c6ba577f70
rakshithaBM <rakshithabm284@gmail.com>
2025-05-06 09:52:14 +0530

    Initial Commit

---
+++ src/main/java/com/gec/hellomaven1/App.java
@@ -0,0 +1,13 @@
+package com.gec.hellomaven1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
